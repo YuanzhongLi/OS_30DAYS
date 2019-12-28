0e1ce039a3608c165792d8e8524eb502b971e7a1
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2019-12-28 11:42:05 +0900

    28

--- HariboteOS/28_day/harib25c/haribote/console.c
+++ HariboteOS/28_day/harib25c/haribote/console.c
@@ -10,7 +10,7 @@ void console_task(struct SHEET *sheet, int memtotal)
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 	int i, *fat = (int *) memman_alloc_4k(memman, 4 * 2880);
 	struct CONSOLE cons;
-	struct FILEHANDLE fhandle[8];
+  struct FILEHANDLE fhandle[8];
 	char cmdline[30];
 
 	cons.sht = sheet;
@@ -25,11 +25,11 @@ void console_task(struct SHEET *sheet, int memtotal)
 		timer_settime(cons.timer, 50);
 	}
 	file_readfat(fat, (unsigned char *) (ADR_DISKIMG + 0x000200));
-	for (i = 0; i < 8; i++) {
-		fhandle[i].buf = 0;	/* 未使用マーク */
-	}
-	task->fhandle = fhandle;
-	task->fat = fat;
+  for (i = 0; i < 8; i++) {
+    fhandle[i].buf = 0;
+  }
+  task->fhandle = fhandle;
+  task->fat = fat;
 
 	/* プロンプト表示 */
 	cons_putchar(&cons, '>', 1);
@@ -101,7 +101,7 @@ void console_task(struct SHEET *sheet, int memtotal)
 			/* カーソル再表示 */
 			if (cons.sht != 0) {
 				if (cons.cur_c >= 0) {
-					boxfill8(cons.sht->buf, cons.sht->bxsize, cons.cur_c, 
+					boxfill8(cons.sht->buf, cons.sht->bxsize, cons.cur_c,
 						cons.cur_x, cons.cur_y, cons.cur_x + 7, cons.cur_y + 15);
 				}
 				sheet_refresh(cons.sht, cons.cur_x, cons.cur_y, cons.cur_x + 8, cons.cur_y + 16);
@@ -392,12 +392,12 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
 					sheet_free(sht);	/* 閉じる */
 				}
 			}
-			for (i = 0; i < 8; i++) {	/* クローズしてないファイルをクローズ */
-				if (task->fhandle[i].buf != 0) {
-					memman_free_4k(memman, (int) task->fhandle[i].buf, task->fhandle[i].size);
-					task->fhandle[i].buf = 0;
-				}
-			}
+      for (i = 0; i < 8; i++) {
+        if (task->fhandle[i].buf != 0) {
+          memman_free_4k(memman, (int) task->fhandle[i].buf, task->fhandle[i].size);
+          task->fhandle[i].buf = 0;
+        }
+      }
 			timer_cancelall(&task->fifo);
 			memman_free_4k(memman, (int) q, segsiz);
 		} else {
@@ -424,9 +424,9 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 		/* reg[0] : EDI,   reg[1] : ESI,   reg[2] : EBP,   reg[3] : ESP */
 		/* reg[4] : EBX,   reg[5] : EDX,   reg[6] : ECX,   reg[7] : EAX */
 	int i;
-	struct FILEINFO *finfo;
-	struct FILEHANDLE *fh;
-	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+  struct FILEINFO *finfo;
+  struct FILEHANDLE *fh;
+  struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 
 	if (edx == 1) {
 		cons_putchar(cons, eax & 0xff, 1);
@@ -543,63 +543,62 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 			io_out8(0x61, (i | 0x03) & 0x0f);
 		}
 	} else if (edx == 21) {
-		for (i = 0; i < 8; i++) {
-			if (task->fhandle[i].buf == 0) {
-				break;
-			}
-		}
-		fh = &task->fhandle[i];
-		reg[7] = 0;
-		if (i < 8) {
-			finfo = file_search((char *) ebx + ds_base,
-					(struct FILEINFO *) (ADR_DISKIMG + 0x002600), 224);
-			if (finfo != 0) {
-				reg[7] = (int) fh;
-				fh->buf = (char *) memman_alloc_4k(memman, finfo->size);
-				fh->size = finfo->size;
-				fh->pos = 0;
-				file_loadfile(finfo->clustno, finfo->size, fh->buf, task->fat, (char *) (ADR_DISKIMG + 0x003e00));
-			}
-		}
-	} else if (edx == 22) {
-		fh = (struct FILEHANDLE *) eax;
-		memman_free_4k(memman, (int) fh->buf, fh->size);
-		fh->buf = 0;
-	} else if (edx == 23) {
-		fh = (struct FILEHANDLE *) eax;
-		if (ecx == 0) {
-			fh->pos = ebx;
-		} else if (ecx == 1) {
-			fh->pos += ebx;
-		} else if (ecx == 2) {
-			fh->pos = fh->size + ebx;
-		}
-		if (fh->pos < 0) {
-			fh->pos = 0;
-		}
-		if (fh->pos > fh->size) {
-			fh->pos = fh->size;
-		}
-	} else if (edx == 24) {
-		fh = (struct FILEHANDLE *) eax;
-		if (ecx == 0) {
-			reg[7] = fh->size;
-		} else if (ecx == 1) {
-			reg[7] = fh->pos;
-		} else if (ecx == 2) {
-			reg[7] = fh->pos - fh->size;
-		}
-	} else if (edx == 25) {
-		fh = (struct FILEHANDLE *) eax;
-		for (i = 0; i < ecx; i++) {
-			if (fh->pos == fh->size) {
-				break;
-			}
-			*((char *) ebx + ds_base + i) = fh->buf[fh->pos];
-			fh->pos++;
-		}
-		reg[7] = i;
-	}
+    for (i = 0; i < 8; i++) {
+      if (task->fhandle[i].buf == 0) {
+        break;
+      }
+    }
+    fh = &task->fhandle[i];
+    reg[7] = 0;
+    if (i < 8) {
+      finfo = file_search((char *) ebx + ds_base, (struct FILEINFO *) (ADR_DISKIMG + 0x002600), 224);
+      if (finfo != 0) {
+        reg[7] = (int) fh;
+        fh->buf = (char *) memman_alloc_4k(memman, finfo->size);
+        fh->size = finfo->size;
+        fh->pos = 0;
+        file_loadfile(finfo->clustno, finfo->size, fh->buf, task->fat, (char *) (ADR_DISKIMG + 0x003e00));
+      }
+    }
+  } else if (edx == 22) {
+    fh = (struct FILEHANDLE *) eax;
+    memman_free_4k(memman, (int) fh->buf, fh->size);
+    fh->buf = 0;
+  } else if (edx == 23) {
+    fh = (struct FILEHANDLE *) eax;
+    if (ecx == 0) {
+      fh->pos = ebx;
+    } else if (ecx == 1) {
+      fh->pos = ebx;
+    } else if (ecx == 2) {
+      fh->pos = fh->size + ebx;
+    }
+    if (fh->pos < 0) {
+      fh->pos = 0;
+    }
+    if (fh->pos > fh->size) {
+      fh->pos = fh->size;
+    }
+  } else if (edx == 24) {
+    fh = (struct FILEHANDLE *) eax;
+    if (ecx == 0) {
+      reg[7] = fh->size;
+    } else if (ecx == 1) {
+      reg[7] = fh->pos;
+    } else if (ecx == 2) {
+      reg[7] = fh->pos - fh->size;
+    }
+  } else if (edx == 25) {
+    fh = (struct FILEHANDLE *) eax;
+    for (i = 0; i < ecx; i++) {
+      if (fh->pos == fh->size) {
+        break;
+      }
+      *((char *) ebx + ds_base + i) = fh->buf[fh->pos];
+      fh->pos++;
+    }
+    reg[7] = i;
+  }
 	return 0;
 }
 
